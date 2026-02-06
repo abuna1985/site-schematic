8349f75fdfd6e957e8956c33f8fad14bc13d788e
Adam Abundis <adamjabundis@gmail.com>
2026-02-06 10:22:54 -0800

    feat: implement header and supress hydration warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Header />
+      <p>Technical Foundation Ready. Start building the BlueprintLayout.</p>
     </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div>[SITE::SCEMATIC]</div>
+      <div>v1.0.0-BETA</div>
+    </header>
+  )
+}
